729391aff5547ba246fc240e5b8616f365c98290
sophia paulin <sobiyamary622@gmail.com>
2024-07-10 04:29:31 +0530

    task

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-light text-center py-3">
+      <div className="container">
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
